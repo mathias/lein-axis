ca3b409c48db688f1982550a414e7bfdfc19d2f5
Matt Gauger <matt.gauger@gmail.com>
2013-11-11 12:10:20 -0600

    Update Clojure; remove contrib dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject lein-axis "0.2"
   :description "Simple Leiningen plug-in to speed up working with Apache Axis and WSDL files."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [axis/axis "1.4"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [axis/axis "1.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :eval-in-leiningen true
   )
